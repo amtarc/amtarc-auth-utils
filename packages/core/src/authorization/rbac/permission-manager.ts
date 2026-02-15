434555dea031c1690b15f22818157f7375f0950a
amir <shapkotaamir0@gmail.com>
2026-02-16 00:24:21 +0545

    fix(authorization): refine permission ID generation by trimming leading and trailing colons

--- packages/core/src/authorization/rbac/permission-manager.ts
+++ packages/core/src/authorization/rbac/permission-manager.ts
@@ -134,6 +134,7 @@ export class PermissionManager {
     return name
       .toLowerCase()
       .replace(/[^a-z0-9]+/g, ':')
-      .replace(/^:+|:+$/g, '');
+      .replace(/^:+/, '')
+      .replace(/:+$/, '');
   }
 }
